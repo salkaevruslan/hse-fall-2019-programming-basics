2309c13b016b3cb9d3c0b64e39fdca6e6f4cdaf4
Ann <annikura@yandex.ru>
2019-11-14 17:08:13 +0300

    Add refactored code

--- 1.5/refactoring_after.py
+++ 1.5/refactoring_after.py
@@ -2,56 +2,49 @@ from typing import List, Any
 
 
 def foo1(a: int, b: int) -> List[int]:
-    result = []
-    for i in range(a, b):
-        result.append(i)
-    return result
+    return [i for i in range(a, b)] # [a, b)
 
 
 def foo2(a: List[int], b: List[int]) -> None:
-    length = len(a)
-    for i in range(length):
-        print(a[i], b[i])
+    for a1, b1 in zip(a, b):
+        print(a1, b1)
 
 
 def foo3(cond: bool, a: int, b: int) -> int:
-    if cond:
-        result = a * 2
-    else:
-        result = b * 3
-    return result
-
+    return a * 2 if cond else b * 3
 
 def foo4(a: Any, b: Any) -> bool:
-    if a is None or b is None or a == b:
-        return True
-    else:
-        return False
+    return a is None or b is None or a == b
 
-
-def foo5(cond: bool, a: int, b: int) -> int:
+def foo5(cond: bool, cond1: bool, a: int, b: int) -> int:
+    result = b
     if cond:
-        return a * b
-    else:
-        return b
-
+        result = b * a
+    if cond1:
+        result = a + b
+    return result
 
 def foo6(cond1: bool, cond2: bool, a: int, b: int) -> None:
     if cond1:
-        if cond2:
-            print(a + b)
-        else:
-            print("a + b:", a + b)
+        result = a + b
+        operation = '+'
     else:
-        if cond2:
-            print(a * b)
-        else:
-            print("a * b:", a * b)
+        result = a * b
+        operation = '*'
+
+    print(result if cond2 else f'a {operation} b: {result}')
+
+    if not cond2:
+        print(f'a {operation} b:', end = '')
+    print(result)
 
 
 def foo7(a: List[int]) -> None:
-    for i in range(len(a)):
-        print(i, a[i])
+    for el, i in enumerate(a):
+        print(i, el)
+
+def foo8(name: str, a: int):
+    "Hi {}. You are {} now".format(name, a)
 
 
 n, m = 3, 6
@@ -59,17 +52,18 @@ matrix = ["#?##..",
           "##..#.",
           "##..#?"]
 
+def is_neighbourghood_empty(x, y, matrix, n, m):
+    deltas = [( 0,  0),
+              (-1,  0),
+              ( 1,  0),
+              ( 0, -1),
+              ( 0,  1)]
+    for dx, dy in deltas:
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < n and \
+           0 <= new_y < m and \
+           matrix[new_x][new_y] != '.':
+                return False
+    return True
 
-def is_neighbourghood_empty(x, y):
-    result = True
-    if matrix[x][y] == '#' or matrix[x][y] == '?':
-        result = False
-    if x - 1 > 0 and (matrix[x - 1][y] == '#' or matrix[x - 1][y] == '?'):
-        result = False
-    if y - 1 > 0 and (matrix[x][y - 1] == '#' or matrix[x][y - 1] == '?'):
-        result = False
-    if y + 1 < m and (matrix[x][y + 1] == '#' or matrix[x][y + 1] == '?'):
-        result = False
-    if x + 1 < n and (matrix[x + 1][y] == '#' or matrix[x + 1][y] == '?'):
-        result = False
-    return result
+print(is_neighbourghood_empty(0, 5, matrix, n, m))
