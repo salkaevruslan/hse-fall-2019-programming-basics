d6866d67396d8552af00b64b7614d124eeb9011e
Ann <annikura@yandex.ru>
2019-11-15 10:42:21 +0300

    Delete 1, 2, 1.5

--- 1/coding/tests_example/division.py
+++
@@ -1,9 +0,0 @@
-"""
-    This file implements division operation
-"""
-
-def division(divisor: float, divider: float) -> float:
-    """
-        Divides divisor by divider
-    """
-    return divisor / divider
